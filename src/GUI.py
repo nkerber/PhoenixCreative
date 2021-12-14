612d35df0d4aea8a4faf5537243332925397f4cd
nkerber23 <nkerber23@my.whitworth.edu>
2021-12-13 22:04:46 -0800

    Connected PDF API, added multithreading

--- src/GUI.py
+++ src/GUI.py
@@ -1,16 +1,53 @@
 import sys
 from PyQt5.uic import loadUi
-from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import *#QDialog, QApplication, QStackedWidget, QWidget
+from PyQt5.QtCore import Qt, QThread, QObject, pyqtSignal
+import PyQt5.QtGui as QtGui
 from ConsoleApp import *
 from decimal import Decimal
+from ExtractFunction import *
 
 #global variable for outputting correct numbers
 #dictionary keys are the index of the measurementSelector dropdown menu,
 #they correspond to the correct multiplier for the unit of measure of pints
-unitMultiplier = {5:1,4:-1,3:-1,2:-1,1:-1,0:-1}
+unitMultiplier = {5:1,4:1.5,3:2,2:4,1:6,0:8}
 currentUnit = 5
 
+class UploadDialog(QDialog):
+    def __init__(self):
+        super(UploadDialog,self).__init__()
+        loadUi('src\\UploadDialog.ui',self)
+
+class Worker(QObject):
+    finished = pyqtSignal()
+    progress = pyqtSignal(int)
+    fileName = pyqtSignal(str)
+    files = ""
+
+    def __init__(self, f):
+        super(Worker,self).__init__()
+        self.files = f
+
+    def run(self):
+        current = 0
+        for f in self.files:
+            print(f)
+            current += 1
+            if ".pdf" in f:
+                self.fileName.emit(f)
+                to_upload = extractPDF(f)
+                addFormula(crsr, to_upload[0][2], to_upload[0][1], to_upload[0][0])
+                old = to_upload[0]
+                to_upload = to_upload[1:]
+                for i in to_upload:
+                    try:
+                        addComponent(crsr, i[0], i[1])
+                        addMakeupElement(crsr, old[2], old[1], i[0],i[2])
+                    except(Exception):
+                        print()
+            self.progress.emit(current)
+        self.finished.emit()
+    
 
 class MainScreen(QMainWindow):
     def __init__(self):
@@ -35,10 +72,99 @@ class UploadScreen(QWidget):
         loadUi('src\\UploadWidget.ui',self)
         #self.login.clicked.connect(self.gotoLogin)
 
+        self.uploadButton.clicked.connect(self.importFiles)
+
+    def dragEnterEvent(self, event: QtGui.QDragEnterEvent):
+        if event.mimeData().hasUrls():
+            event.accept()
+        else:
+            event.ignore()
+            print("Ignoring")
+    
+    def dragMoveEvent(self, event: QtGui.QDragMoveEvent):
+        if event.mimeData().hasUrls():
+            event.setDropAction(Qt.DropAction.CopyAction)
+            event.accept()
+        else:
+            event.ignore()
+            print("Ignoring")
+
+    def dropEvent(self, event: QtGui.QDropEvent) -> None:
+        if event.mimeData().hasUrls():
+            event.setDropAction(Qt.DropAction.CopyAction)
+            event.accept()
+            links = []
+
+            for url in event.mimeData().urls():
+                if url.isLocalFile():
+                    links.append(str(url.toLocalFile()))
+            
+            newDialog = UploadDialog()
+            newDialog.setFixedHeight(150)
+            newDialog.setFixedWidth(300)
+            newDialog.show()
+            newDialog.currentFormula.setText("Demo Formula Name2")
+            newDialog.progressBar.setRange(0, len(links))
+
+            thread = QThread()
+            worker = Worker(links)
+            worker.moveToThread(thread)
+
+            thread.started.connect(worker.run)
+            worker.finished.connect(thread.quit)
+            worker.finished.connect(worker.deleteLater)
+            thread.finished.connect(thread.deleteLater)
+            worker.progress.connect(newDialog.progressBar.setValue)
+            worker.fileName.connect(newDialog.currentFormula.setText)
+
+            thread.start()
+            
+            try:
+                sys.exit(newDialog.exec())
+            except:
+                print("")
+
+    def importFiles(self):
+
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        files, _ = QFileDialog.getOpenFileNames(self,"QFileDialog.getOpenFileNames()", "","PDF Files (*.pdf)", options=options)
+        
+        newDialog = UploadDialog()
+        newDialog.setFixedHeight(150)
+        newDialog.setFixedWidth(300)
+        newDialog.show()
+        newDialog.currentFormula.setText("Demo Formula Name2")
+        newDialog.progressBar.setRange(0, len(files))
+
+        thread = QThread()
+        worker = Worker(files)
+        worker.moveToThread(thread)
+
+        thread.started.connect(worker.run)
+        worker.finished.connect(thread.quit)
+        worker.finished.connect(worker.deleteLater)
+        thread.finished.connect(thread.deleteLater)
+        worker.progress.connect(newDialog.progressBar.setValue)
+        worker.fileName.connect(newDialog.currentFormula.setText)
+
+        thread.start()
+        try:
+            sys.exit(newDialog.exec())
+        except:
+            print("")
+
 class HomeScreen(QWidget):
     def __init__(self):
         super(HomeScreen,self).__init__()
         loadUi('src\\HomeWidget.ui',self)
+
+        #is there a better way to do this without this pointless loop?
+        for row in componentCount(crsr):
+            self.numComponents.setText(str(row.Count))
+        
+        for row in formulaCount(crsr):
+            self.numFormulas.setText(str(row.Count))
 
 class FormulaScreen(QWidget):
     def __init__(self):
@@ -86,14 +212,16 @@ class FormulaScreen(QWidget):
         self.makeupTable.clearContents()
         currentRow = 0
 
+        factor = Decimal(unitMultiplier[currentUnit])
+
         for element in makeupElements:
             self.makeupTable.setItem(currentRow,0,QTableWidgetItem(element.CompCode))
             self.makeupTable.setItem(currentRow,1,QTableWidgetItem(element.IntDesc.title()))
-            self.makeupTable.setItem(currentRow,2,QTableWidgetItem(str(unitMultiplier[currentUnit]*element.GramsPerPint)))
+            self.makeupTable.setItem(currentRow,2,QTableWidgetItem(str(factor*element.GramsPerPint)))
             if not currentRow == 0:
-                self.makeupTable.setItem(currentRow,3,QTableWidgetItem(str(unitMultiplier[currentUnit]*element.GramsPerPint+Decimal(self.makeupTable.item(currentRow-1,3).text()))))
+                self.makeupTable.setItem(currentRow,3,QTableWidgetItem(str(factor*element.GramsPerPint+Decimal(self.makeupTable.item(currentRow-1,3).text()))))
             else:
-                self.makeupTable.setItem(currentRow,3,QTableWidgetItem(str(unitMultiplier[currentUnit]*element.GramsPerPint)))
+                self.makeupTable.setItem(currentRow,3,QTableWidgetItem(str(factor*element.GramsPerPint)))
             currentRow += 1
 
     def updateSelection(self):
