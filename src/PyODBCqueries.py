e2f3262e50c6d73365a74f4fb08260035caf64f8
nkerber23 <nkerber23@my.whitworth.edu>
2021-12-15 23:55:46 -0800

    New component queries

--- src/PyODBCqueries.py
+++ src/PyODBCqueries.py
@@ -17,8 +17,7 @@ def getNumVersions(formNum):
     sql += "WHERE MPNum = "+str(formNum)+" "
     sql+= "ORDER BY Version DESC"
     print("Trying sql: ",sql)
-    rows = connectB.execute(sql)
-    return rows
+    return connectB.execute(sql)
 
 def getMakeupElements(formCode,version):
     sql =  "SELECT M.CompCode,C.IntDesc,M.GramsPerPint " 
@@ -26,15 +25,13 @@ def getMakeupElements(formCode,version):
     sql += "WHERE FormNum = " + str(formCode) + " AND FormVersion = " + str(version) +" AND M.CompCode = C.IntCode "
     sql += "ORDER BY GramsPerPint DESC"
     #print("trying sql:",sql)
-    rows = connectA.execute(sql)
-    return rows
+    return connectA.execute(sql)
 
 def getAllFormulas():
     sql = "SELECT F1.FormName, F2.Version, F2.MPNum "
     sql +="FROM Formula as F1, (SELECT MAX(F.Version) as Version, F3.MPNum as MPNum FROM Formula F, (SELECT DISTINCT MPNum From Formula) as F3 WHERE F.MPNum = F3.MPNum GROUP BY F3.MPNum) as F2 "
     sql +="WHERE F1.MPNum = F2.MPNum and F1.Version = F2.Version"
-    rows = connectA.execute(sql)
-    return rows
+    return connectA.execute(sql)
 
 def componentCount():
     sql = "SELECT COUNT(*) AS Count "
@@ -51,14 +48,38 @@ def formulaCount():
 def getFormula(formNum,version):
     sql = "SELECT * "
     sql += "FROM Formula "
-    sql += "WHERE MPNum = '"+formNum+"' AND Version = "+version
+    sql += "WHERE MPNum = "+formNum+" AND Version = "+version+""
+    print("Grabbing formulas via SQL: " + sql)
+    return connectA.execute(sql)
+
+def getComponent(compCode):
+    sql = "SELECT * "
+    sql += "FROM Component "
+    sql += "WHERE IntCode = '"+compCode+"'"
+    return connectA.execute(sql)
+
+def getAllComponents():
+    sql =  "SELECT * "
+    sql += "FROM Component "
+    return connectA.execute(sql)
+
+def componentSearch(param):
+    sql =  "SELECT * "
+    sql += "FROM Component "
+    sql += "WHERE IntCode LIKE '%"+str(param)+"%' or IntDesc like '%"+str(param)+"%'"
+    return connectA.execute(sql)
+
+def getFormulasFromComponent(compCode):
+    sql =  "SELECT F.MPNum,F.FormName,F.Version, M.GramsPerPint "
+    sql += "FROM Component C, Formula F, Makeup M "
+    sql += "WHERE C.IntCode = M.CompCode AND F.MPNum = M.FormNum AND C.IntCode = '"+str(compCode)+"'"
     return connectA.execute(sql)
 
 def deepFormulaSearch(param):
     sql =  "SELECT DISTINCT Formula.MPNum, Formula.Version, CAST(Formula.FormName AS VARCHAR(MAX)) as FormName, CAST(Formula.Notes AS VARCHAR(MAX)) as Notes "
     sql += "FROM Formula, Makeup, Component,(SELECT MAX(F.Version) as Version, F3.MPNum as MPNum FROM Formula F, (SELECT DISTINCT MPNum From Formula) as F3 WHERE F.MPNum = F3.MPNum GROUP BY F3.MPNum) as F2 "
     sql += "WHERE Formula.MPNum = Makeup.FormNum AND Makeup.CompCode = Component.IntCode AND Formula.Version = F2.Version AND Formula.MPNum = F2.MPNum AND "
-    sql += "(Component.IntDesc LIKE '%"+str(param) +"%' OR Formula.FormName LIKE '%"+str(param)+"%' OR Formula.MPNum LIKE '%"+str(param)+"%' OR Formula.Version LIKE '%"+str(param)+"%' OR Formula.Notes LIKE '%"+str(param)+"%')"
+    sql += "(Component.IntDesc LIKE '%"+str(param) +"%' OR Formula.FormName LIKE '%"+str(param)+"%' OR Formula.MPNum LIKE '%"+str(param)+"%' OR Formula.Version LIKE '%"+str(param)+"%' OR Formula.Notes LIKE '%"+str(param)+"%' OR Component.IntCode LIKE '%"+str(param)+"%')"
     return connectA.execute(sql)
 
 def shallowFormulaSearch(param):
@@ -66,6 +87,7 @@ def shallowFormulaSearch(param):
     sql += "FROM Formula,(SELECT MAX(F.Version) as Version, F3.MPNum as MPNum FROM Formula F, (SELECT DISTINCT MPNum From Formula) as F3 WHERE F.MPNum = F3.MPNum GROUP BY F3.MPNum) as F2 "
     sql += "WHERE Formula.Version = F2.Version AND Formula.MPNum = F2.MPNum AND "
     sql += "(Formula.FormName LIKE '%"+str(param)+"%' OR Formula.MPNum LIKE '%"+str(param)+"%' OR Formula.Version LIKE '%"+str(param)+"%' OR Formula.Notes LIKE '%"+str(param)+"%')"
+    print("Shallow searching\n\n")
     return connectA.execute(sql)
 
 def addComponent(code,desc):
@@ -87,7 +109,6 @@ def addMakeupElement(formNum,Version,compCode,GpP):
     print("Updated makeup for",str(formNum),str(Version),"and component",compCode,"successfully.")
 
 def addFormula( formNum,formVersion,formName,fileName):
-    #fileName = "NULL"
     sql =  "IF EXISTS (SELECT MPNum FROM Formula WHERE MPNum = '"+str(formNum)+"' AND Version = '"+str(formVersion)+"')"
     sql += "UPDATE Formula SET FormName = '"+formName+"' WHERE MPNum = '"+str(formNum)+"' AND Version = '"+str(formVersion)+"'"
     sql += "ELSE INSERT INTO Formula VALUES ('"+str(formNum)+"','"+str(formVersion)+"','"+formName+"','"+fileName+"')"
